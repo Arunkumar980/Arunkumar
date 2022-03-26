f096e5bdd2972ece4d198a33609d836fd03e8614
Arunkumar980 <arunk344556@gmail.com>
2022-03-26 19:49:36 +0530

    arun

---
+++ src/org/test/Employee.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Employee {
+	
+	public void gethub() {
+
+		System.out.println("employee......");
+	}
+	public static void main(String[] args) {
+		
+	
+Employee employee=new Employee();
+employee.gethub();
+}
+}
